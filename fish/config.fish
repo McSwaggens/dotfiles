68621619ef996de45eacc0f05e39deaa25b0d83c
McSwaggens <McSwaggens@protonmail.com>
2024-11-15 20:20:07 +1100

    Updated fish configs

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,25 @@
-alias gst  'git status'
-alias clr  'clear'
-alias uefi 'systemctl reboot --firmware-setup'
-alias scan 'rg --vimgrep "f_op" | kak -e "set buffer filetype make"'
+alias gst    'git status'
+alias co     'git checkout'
+alias clr    'clear'
+alias uefi   'systemctl reboot --firmware-setup'
+alias config 'kak ~/.config/fish/config.fish'
+
+function xx
+	command kak -e "grep $argv"
+end
+
+function d
+	command kak -e "git diff $argv"
+end
 
 function man
-	command man $argv | kak -e 'set buffer filetype man'
+	command kak -e "man $argv"
+end
+
+function fish_prompt
+	string join '' -- (set_color green) (prompt_pwd --full-length-dirs 2) (set_color normal) ' > '
 end
 
 export EDITOR=kak
 export MOZ_ENABLE_WAYLAND=1
+
--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -12,6 +12,9 @@ function fish_user_key_bindings
     bind \ca beginning-of-line
     bind \ce end-of-line
 
+    bind \cz fg
+    bind \cf _fzf_search_directory
+
     bind -k backspace backward-delete-char
     bind \eH backward-kill-word
 
