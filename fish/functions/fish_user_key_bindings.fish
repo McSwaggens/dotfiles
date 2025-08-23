dcd3d459944102009c87c8bbf5e7fed10c72e11c
McSwaggens <McSwaggens@protonmail.com>
2025-08-23 10:51:14 +1000

    clear screen with ctrl-l

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -7,16 +7,23 @@ function fish_user_key_bindings
     bind \cj down-or-search
 
     bind \ck up-or-search
-    bind \cl forward-char
+    bind \cl tryclear
 
     bind \ca beginning-of-line
     bind \ce end-of-line
 
     bind \cz fg
-    bind \cf _fzf_search_directory
 
     bind -k backspace backward-delete-char
     bind \eH backward-kill-word
 
     bind --erase --preset \cd
+end
+
+function tryclear
+	if test -z (commandline)
+		commandline -f clear-screen
+	else
+		commandline -f forward-char
+	end
 end
